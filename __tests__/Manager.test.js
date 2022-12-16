be3aa7a4e999c37e3ff825ef6420937fc7eabd14
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2022-12-15 20:34:52 -0500

    Readme, video and final files added

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,20 +1,20 @@
-const Manager = require('./lib/Manager');
-const Employee = require('./lib/Employee');
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
 
 test('creates a manager object', () => {
-    const manager = new Manager('Manager#1');
+    const manager = new Manager('Manager1');
 
     expect(typeof(manager)).toBe('object');
 });
 
 test('gets office number', () => {
-    const manager = new Manager('Manager', '1', 'carlos@email.fr', '4');
+    const manager = new Manager('Manager', '1', 'noemiegrau@outlook.fr', '6');
 
-    expect(manager.getOfficeNumber()).toBe('4');
+    expect(manager.getOfficeNumber()).toBe('6');
 });
 
-test('getRole() return Manager', () => {
+test('getRole() returns Manager', () => {
     const manager = new Manager('Manager');
-    
+
     expect(manager.getRole()).toBe('Manager');
 });
